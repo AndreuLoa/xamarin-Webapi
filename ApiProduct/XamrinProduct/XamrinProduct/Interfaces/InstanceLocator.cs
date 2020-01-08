8431bdd11a3ada910c91fade50b2a5e00ad8ffcd
Andreu Loayza <andreu.loayza@hotmail.com>
2020-01-07 20:46:32 -0400

    commit ya enlazando

---
+++ ApiProduct/XamrinProduct/XamrinProduct/Interfaces/InstanceLocator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using XamrinProduct.ViewModels;
+
+namespace XamrinProduct.Interfaces
+{
+    public class InstanceLocator
+    {
+        #region Properties
+        public MainViewModel Main { get; set; }
+        #endregion        
+
+        #region Constructors
+        public InstanceLocator()
+        {
+            this.Main = new MainViewModel();
+        }
+        #endregion
+    }
+}
