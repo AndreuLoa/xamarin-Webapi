d7f357429267f8c8f2fa18330605e172093efe56
Andreu Loayza <andreu.loayza@hotmail.com>
2020-01-08 01:35:37 -0400

    commit viewmodel almost done

--- ApiProduct/XamrinProduct/XamrinProduct/ViewModels/LoginViewModel.cs
+++ ApiProduct/XamrinProduct/XamrinProduct/ViewModels/LoginViewModel.cs
@@ -1,10 +1,100 @@
-﻿using System;
+﻿using GalaSoft.MvvmLight.Command;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Input;
 
 namespace XamrinProduct.ViewModels
 {
-    class LoginViewModel
+    class LoginViewModel:BaseViewModel
     {
+        #region Attributes
+        string email;
+        string password;
+        bool isrunning;
+        bool isenabled;
+
+        #endregion
+
+        #region Properties
+        public string Email
+        {
+            get
+            {
+                return this.email;
+            }
+            set
+            {
+                SetValue(ref this.email, value);
+            }
+        }
+        public string Password
+        {
+            get
+            {
+                return this.password;
+            }
+            set
+            {
+                SetValue(ref this.password, value);
+            }
+        }
+        public bool IsRunning
+        {
+            get
+            {
+                return this.isrunning;
+            }
+            set
+            {
+                SetValue(ref this.isrunning, value);
+            }
+        }
+        public bool IsEnabled
+        {
+            get
+            {
+                return this.isrunning;
+            }
+            set
+            {
+                SetValue(ref this.isrunning, value);
+            }
+        }
+        #endregion
+
+        #region Commands
+        public ICommand LoginCommand
+        {
+            get
+            {
+                return new RelayCommand(cmdLogin);
+            }
+        }
+
+        private async void cmdLogin()
+        {
+            if (string.IsNullOrEmpty(Email))
+            {
+                await App.Current.MainPage.DisplayAlert("Email empty",
+                                            "Enter a Valid E-mail",
+                                            "Accept");
+                return;
+            }
+            if (string.IsNullOrEmpty(Password))
+            {
+                await App.Current.MainPage.DisplayAlert("Password empty",
+                                            "Enter a Valid Password",
+                                            "Accept");
+                return;
+            }
+
+            IsRunning = true;
+            IsEnabled = false;
+
+           
+        }
+        #endregion
+
     }
 }
